475901c415ef919e36315c13d617f0c1fd5143f0
ChiaraMameli <mameli.chiara@libero.it>
2022-07-21 16:17:25 +0200

    created title

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <MyTitle />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyTitle from './components/MyTitle.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MyTitle: MyTitle ,
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container{
+  width: 80%;
+  margin: 0 auto;
 }
 </style>
